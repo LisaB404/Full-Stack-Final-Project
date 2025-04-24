7bf14aba08e283e62b14a03805b6ac9841648070
Lisa <lizardb96@gmail.com>
2025-04-24 19:28:12 +0200

    split routes and controllers

--- server/routes/libraryRoutes.js
+++ server/routes/libraryRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/userModel");
+const libraryController = require("../controllers/libraryController");
 
 // Middleware for JWT authentication
 const authenticateJWT = require("../middleware/authenticateJWT");
@@ -7,61 +7,12 @@ const authenticateJWT = require("../middleware/authenticateJWT");
 const router = express.Router();
 
 // GET LIBRARY
-router.get("/library", authenticateJWT, async (req, res) => {
-  try {
-    const user = await User.findById(req.userId);
-    if (!user) return res.status(404).json({ message: "User not found" });
-    res.json({ library: user.library });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
+router.get("/library", authenticateJWT, libraryController.getLibrary);
 
 // ADD BOOK TO LIBRARY
-router.post("/library/add", authenticateJWT, async (req, res) => {
-  try {
-    const userId = req.userId;
-    const { book } = req.body;
-    // Find user by ID
-    const user = await User.findById(userId);
-    if (!user) return res.status(404).json({ message: "User not found" });
-    // Check if book is already in the library
-    if (user.library.find((b) => b.id === book.id)) {
-      return res.status(400).json({ message: "Book already in library" });
-    }
-    // Add book to library
-    user.library.push(book);
-    await user.save();
-    res
-      .status(200)
-      .json({ message: "Book added successfully", library: user.library });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
+router.post("/library/add", authenticateJWT, libraryController.addBookToLibrary);
 
 // REMOVE BOOK FROM LIBRARY
-router.post("/library/remove", authenticateJWT, async (req, res) => {
-  try {
-    const userId = req.userId;
-    const { bookTitle } = req.body;
-
-    const user = await User.findById(userId);
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-    // Filter out book to remove
-    user.library = user.library.filter((book) => book.id !== bookTitle);
-    await user.save();
-
-    res
-      .status(200)
-      .json({ message: "Book removed successfully", library: user.library });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
+router.post("/library/remove", authenticateJWT, libraryController.removeBookFromLibrary);
 
 module.exports = router;
